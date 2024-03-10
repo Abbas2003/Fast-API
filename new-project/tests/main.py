0ad958dcaf2f7c0af284578393a7029c021d4839
Muhammad Qasim <mohommadqasim1111@gmail.com>
2024-03-10 17:55:39 +0500

    GET() method

--- new-project/tests/main.py
+++ new-project/tests/main.py
@@ -1,6 +1,6 @@
 # fastapi_neon/main.py
 
-from fastapi import FastAPI
+from fastapi import FastAPI,Path
 
 app = FastAPI(title="Hello World API", 
     version="0.0.1",
@@ -11,19 +11,33 @@ app = FastAPI(title="Hello World API",
         }
         ])
 
-students = [
+students = {
     1: {
         "name": "Abbas",
         "age": 20,
         "class": "2 year CS",
+    },
+    2: {
+        "name": "Aqsa",
+        "age": "Pata Nhi",
+        "class": "Mommy",
+    },
+    3: {
+        "name": "Aftab",
+        "age": "Main nh bataonga",
+        "class": "Daddy",
     }
-]
+}
 
 
 @app.get("/")
 def read_root():
     return {"Hello": "Fast API"}
 
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return {"item_id": item_id}
+
 @app.get("/get-student/{student_id}")
-def get_student(student_id: int = Path(None, description="The ID of the student you want to view")):
+def get_student(student_id: int ):
     return students[student_id]
