97950381bebdd7e540658c27d38e07ad517c890a
Muhammad Qasim <mohommadqasim1111@gmail.com>
2024-03-13 21:54:19 +0500

    commit

--- new-project/tests/main.py
+++ new-project/tests/main.py
@@ -1,6 +1,7 @@
 # fastapi_neon/main.py
 
 from fastapi import FastAPI,Path
+from enum import Enum
 
 app = FastAPI(title="Hello World API", 
     version="0.0.1",
@@ -28,6 +29,21 @@ students = {
         "class": "Daddy",
     }
 }
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
 
 
 @app.get("/")
